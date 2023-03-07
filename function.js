fa1666c1f878b0a1bc87eeeddd18e9dd70716f4e
mbarrera71 <126711362+mbarrera71@users.noreply.github.com>
2023-03-07 19:50:41 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -1,16 +1,20 @@
 
 document.getElementById('boton1').onclick = function () {
-    document.getElementById("dato").innerHTML = "Soltero";
+    document.getElementById("dato").innerHTML = "Poseo excelentes habilidades de análisis, planificación y trabajo en equipo que he adquirido durante mis estudios universitarios; las habilidades en planificación estratégica y mis trabajos desempeñados en las distintas empresas me dieron las herramientas necesarias para poder desempeñarme con eficiencia en el mundo laboral actual.";
 };
 
 document.getElementById('boton2').onclick = function () {
-    document.getElementById("dato").innerHTML = "Part Time";
+    document.getElementById("dato").innerHTML = "Soltero";
 };
 
 document.getElementById('boton3').onclick = function () {
-    document.getElementById("dato").innerHTML = "D4 - vigente 2028";
+    document.getElementById("dato").innerHTML = "Part Time / Full Time";
 };
 
-document.getElementById('ocutar_info').addEventListener('click', function() {
+document.getElementById('boton4').onclick = function () {
+    document.getElementById("dato").innerHTML = "D4 - vigente hasta Junio 2028";
+};
+
+document.getElementById('ocultar_info').addEventListener('click', function() {
     document.getElementById("dato").style.display = 'none';
 });
